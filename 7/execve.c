3917b0fee860f6eb6f5b5e0fcab5cacb48dce8e4
Thedawn <315843093@qq.com>
2021-07-20 07:45:19 +0800

    "不知道什么会走execve分支"

--- 7/execve.c
+++ 7/execve.c
@@ -4,7 +4,7 @@
 extern char** environ;
 
 /*
- @brief 测试execve函数
+ @brief 测试execve函数,需要有new.c文件产生的new,会走else分支;
  * */
 int main(int argc, char* argv[])
 {
@@ -12,7 +12,7 @@ int main(int argc, char* argv[])
 	if((pid = fork()) < 0)
 		puts("create child process failed!");
 	if(pid == 0)
-		execve("new", argv, environ);
+		execve("new", argv, environ);//不明白什么子进程会走execve分支
 	else
 		puts("此信息正常输出!");
 	return 0;
